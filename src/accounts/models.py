d2c5b76f5442e2de55d485c76fbd7b353690644a
gabigab117 <gabrieltrouve5@outlook.com>
2023-02-21 23:26:28 +0100

    Initial commit

---
+++ src/accounts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
+from profil import settings
+
+
+# pour rajouter des champs on passe par l'héritage de AbstractUser normalement.
+# pourquoi utiliser un modele avec OneToOne ?
+class CustomUser(AbstractUser):
+    pass
+
+
+# Dans le cas où je veux différentes données pour différents types d'utilisateurs. Ex un client, un vendeur
+class Profile(models.Model):
+    # on ne met pas directement la class mais on passe par le fichier de settings
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    vendeur_id = models.CharField(max_length=5)
+
+
+def post_save_receiver(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+# relier la fonction post_save_receiver à l'émetteur AUTHUSERMODEL
+post_save.connect(post_save_receiver, sender=settings.AUTH_USER_MODEL)

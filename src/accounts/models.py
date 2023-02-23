6a60243dff83d1b0ea1862b093a56ee5208893bf
gabigab117 <gabrieltrouve5@outlook.com>
2023-02-23 23:12:10 +0100

    maj

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -14,7 +14,6 @@ class CustomUser(AbstractUser):
 class Profile(models.Model):
     # on ne met pas directement la class mais on passe par le fichier de settings
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    vendeur_id = models.CharField(max_length=5)
 
 
 def post_save_receiver(sender, instance, created, **kwargs):
@@ -24,3 +23,4 @@ def post_save_receiver(sender, instance, created, **kwargs):
 
 # relier la fonction post_save_receiver à l'émetteur AUTHUSERMODEL
 post_save.connect(post_save_receiver, sender=settings.AUTH_USER_MODEL)
+# qd je crée une instance de CustomUser la fonction post_save_receiver est exécutée
